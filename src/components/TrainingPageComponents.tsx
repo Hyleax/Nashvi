ecdcab9112c4055beca50a970a83b09938fc34da
Hyleax <normanyaptw@yahoo.com>
2024-02-04 23:10:09 +0800

    remove audio test - 3

--- src/components/TrainingPageComponents.tsx
+++ src/components/TrainingPageComponents.tsx
@@ -8,8 +8,35 @@ import useChordQuestion from "@/hooks/useChordQuestion";
 import { useEffect, useState } from "react";
 import { checkCorrectChord } from "@/utils/chords";
 import { playSample } from "@/utils/samples";
-import * as Tone from "tone"
 
+
+// tonejs stuff
+import * as Tone from "tone"
+const toneObject = Tone
+const notes =  {
+  "C3": "C3.mp3",
+  "C#3": "Cs3.mp3",
+  "Db3": "Cs3.mp3",
+  "D3": "D3.mp3",
+  "D#3": "Ds3.mp3",
+  "Eb3": "Ds3.mp3",
+  "E3": "E3.mp3",
+  "F3": "F3.mp3",
+  "F#3": "Fs3.mp3",
+  "Gb3": "Fs3.mp3",
+  "G3": "G3.mp3",
+  "G#3": "Gs3.mp3",
+  "Ab3": "Gs3.mp3",
+  "A3": "A3.mp3",
+  "Bb3": "As3.mp3",
+  "B3": "B3.mp3",
+}
+
+const piano = new toneObject.Sampler({
+  urls: notes,
+  release: 1,
+  baseUrl: '/samples/piano/'
+}).toDestination()
 
 const pinkBG = "rounded-xl shadow-lg bg-pink-400 hover:bg-pink-500 p-2 text-3xl font-semibold"
 const orangeBG = "rounded-xl shadow-lg bg-orange-500 hover:bg-orange-600 p-2 text-3xl font-semibold"
